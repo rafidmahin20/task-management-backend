951446d232754d9f6de152fd1377dcda3fe3a415
rafidmahin20 <submanmahin@gmail.com>
2024-03-31 14:36:40 +0600

    updated

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 import express from "express";
 const app = express();
 
-app.get("/", (req, res) =>{
-    res.json({
-        message: "Server Working",
-});
-})
+
 
 export default app;
--- server.js
+++ server.js
@@ -2,6 +2,6 @@ import app from './app.js';
 
 // const PORT = 4000;
 
-app.listen(4000, () =>{
-    console.log(`Server listening on port: ${4000}`);
+app.listen(process.env.PORT, () =>{
+    console.log(`Server listening on port: ${process.env.PORT,}`);
 });
